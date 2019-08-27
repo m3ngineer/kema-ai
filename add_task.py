794fe974291bbd96067fa516220db9781f220081
Matthew Eng <mattheweng@matt.lan>
2019-08-27 08:55:41 -0400

    Change to use twilio client to make call

--- add_task.py
+++ add_task.py
@@ -1,19 +1,23 @@
 import requests
 from requests.auth import HTTPBasicAuth
+from twilio.rest import Client
 
 import conf
 
-def add_task():
+def execute_task():
     params = {
     'To': '+19148198579',
     'From': '+19143369499'
     }
 
-    r = requests.post('https://studio.twilio.com/v1/Flows/FWc0af0c20acf7eabf483e17bea0962bba/Executions',
-        params=params,
-        auth=(conf.twilio_account_sid, conf.twilio_auth_token))
+    client = Client(conf.twilio_account_sid, conf.twilio_auth_token)
 
-    return r
+    execution = client.studio \
+                      .flows('FWc0af0c20acf7eabf483e17bea0962bba') \
+                      .executions \
+                      .create(to='+19148198579', from_='+19143369499')
+
+    print(execution.sid)
 
 if __name__ == "__main__":
-    print(add_task())
+    print(execute_task())
