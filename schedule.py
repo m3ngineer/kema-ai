1174f194a3ebcf240f8cac3625d67ca50ea3c1b1
Matthwe Eng <matthew.eng2@gmail.com>
2019-08-30 09:02:23 -0400

    Flush out parse_date to work if 'next' or 'today' in text

--- schedule.py
+++ schedule.py
@@ -1,35 +1,56 @@
 import pandas as pd
-from datetime import datetime, now, timedelta
+from datetime import datetime, timedelta
 
 def parse_date(text):
     '''
     Parses date from human-readable text
     '''
 
+    def next_weekday(d, weekday):
+        '''
+            Get the next weekday as a date given a date d
+            weekday: 0=Monday, 1=Tuesday...
+        '''
+
+        days_ahead = weekday - d.weekday()
+        if days_ahead <= 0: # Target day already happened this week
+            days_ahead += 7
+        return d + timedelta(days_ahead)
+
     days = {
-    'mon': ['monday', 'mon'],
-    'tues': ['tuesday', 'tues'],
-    'wed': ['wednesday', 'wed', 'wednes'],
-    'thurs': ['thursday', 'thurs'],
-    'fri': ['friday', 'fri'],
-    'sat': ['saturday', 'sat'],
-    'sun': ['sunday', 'sun'],
+        0: ['monday', 'mon'],
+        1: ['tuesday', 'tues'],
+        2: ['wednesday', 'wed', 'wednes'],
+        3: ['thursday', 'thurs'],
+        4: ['friday', 'fri'],
+        5: ['saturday', 'sat'],
+        6: ['sunday', 'sun'],
     }
 
     text = [word.lower() for word in text.split()]
+    current_date = datetime.now().date()
+
     if 'next' in text:
-        # create min date
-        # assign to next week
-        pass
+        # get the next weekday date
+        for word in text:
+            for weekday_num, weekday_words in days.items():
+                if word in weekday_words:
+                    weekday=weekday_num
+
+        scheduled_date = next_weekday(current_date, weekday)
+
     elif 'today' in text:
         # assign to current date
-        pass
+        scheduled_date = current_date
+
     elif days.any() in text:
         # assign next available date equal to day
+        weekday_phrases = [inner for outer in days.values() for inner in outer]
+
         pass
 
     # return date
-    pass
+    return scheduled_date
 
 def parse_time(text):
     '''
@@ -48,3 +69,10 @@ def schedule_job(dt):
     Schedules a job given a datetime object
     '''
     pass
+
+if __name__ == "__main__":
+
+    tests = ['toay at 4 pm', '4pm today', 'tomorrow', 'today', 'whenever', 'in 4 hours'
+    'end of day', 'eod', 'end of tomorrow', 'tomorrow afternoon']
+    print(parse_date('today at 4 pm'))
+    print(parse_date('next thurs at 4 pm'))
