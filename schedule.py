97b52b4c503e3ce40c46de03fafcb224bb73f55c
Matthwe Eng <matthew.eng2@gmail.com>
2019-08-31 19:35:43 -0400

    Fix parse_time and refactor parse_date; combine functions into class

--- schedule.py
+++ schedule.py
@@ -2,106 +2,127 @@ import pandas as pd
 from datetime import datetime, timedelta
 import re
 
-def parse_date(text):
+class ScheduleJob():
     '''
-    Parses date from human-readable text
+    Takes in human-readable text time and extracts datetime to schedule a job
     '''
+    def __init__(self, text):
+        self.scheduled_datetime = self.parse_datetime(text)
 
-    def next_weekday(d, weekday):
+    def parse_date(self, text):
         '''
-            Get the next weekday as a date given a date d
-            weekday: 0=Monday, 1=Tuesday...
+        Parses date from human-readable text
         '''
 
-        days_ahead = weekday - d.weekday()
-        if days_ahead <= 0: # Target day already happened this week
-            days_ahead += 7
-        return d + timedelta(days_ahead)
+        def next_weekday(d, weekday):
+            '''
+                Get the next weekday as a date given a date d
+                weekday: 0=Monday, 1=Tuesday...
+            '''
+            print(weekday, d)
+            days_ahead = weekday - d.weekday()
+            if days_ahead <= 0: # Target day already happened this week
+                days_ahead += 7
+            return d + timedelta(days_ahead)
 
-    days = {
-        0: ['monday', 'mon'],
-        1: ['tuesday', 'tues'],
-        2: ['wednesday', 'wed', 'wednes'],
-        3: ['thursday', 'thurs'],
-        4: ['friday', 'fri'],
-        5: ['saturday', 'sat'],
-        6: ['sunday', 'sun'],
-    }
+        days = {
+            0: ['monday', 'mon'],
+            1: ['tuesday', 'tues'],
+            2: ['wednesday', 'wed', 'wednes'],
+            3: ['thursday', 'thurs'],
+            4: ['friday', 'fri'],
+            5: ['saturday', 'sat'],
+            6: ['sunday', 'sun'],
+        }
 
-    text = [word.lower() for word in text.split()]
-    current_date = datetime.now().date()
-
-    if 'next' in text:
-        # get the next weekday date
-        for word in text:
-            for weekday_num, weekday_words in days.items():
-                if word in weekday_words:
-                    weekday=weekday_num
-
-        scheduled_date = next_weekday(current_date, weekday)
-
-    elif 'today' in text:
-        # assign to current date
+        text = [word.lower() for word in text.split()]
+        current_date = datetime.now().date()
         scheduled_date = current_date
 
-    elif days in text:
-        # assign next available date equal to day
-        weekday_phrases = [inner for outer in days.values() for inner in outer]
-
-        pass
-
-    # return date
-    return scheduled_date
-
-def parse_time(text):
-    '''
-    Parses time from human-readable text
-    Does not deal with minutes yet
-    '''
-
-    scheduled_time = datetime.strptime('12:00', '%H:%M').time()
-
-    current_time = datetime.now().time()
-
-    try:
-        ints_str = [str(i) for i in list(range(1, 25))]
-        if re.findall('at ?\d+\s?a?p?m?.*', text.lower()):
-            hour =  re.findall('at ?\d+\s?a?p?m?.*', text.lower())
-            hour = hour[0].replace('at', '').replace('pm', '').replace('am', '').replace(' ', '')
-        elif re.findall(' ?\d+ *a?p?m?.*', text.lower()):
-            print(2)
-            hour = re.findall(' ?\d+\s?a?p?m?.*', text.lower())
-            hour = hour[0].replace('at', '').replace('pm', '').replace('am', '').replace(' ', '')
+        if 'today' in text:
+            return scheduled_date
+        elif 'tomorrow' in text:
+            return current_date + timedelta(1)
         else:
-            return scheduled_time
+            weekday_phrases = [inner for outer in days.values() for inner in outer]
+            # get the next weekday date
+            found = False
+            for word in text:
+                for weekday_phrase in weekday_phrases:
+                    if word == weekday_phrase:
+                        for k,v in days.items():
+                            if v == weekday_phrase:
+                                weekday = k
+                                scheduled_date = next_weekday(current_date, weekday)
+                        break
+                if found:
+                    break
 
-        print(hour)
-        if 'pm' in text:
-            scheduled_time = datetime.striptime('{} PM'.format(hour), '%H %p').time()
-        elif 'am' in text:
-            scheduled_time = datetime.striptime('{} AM'.format(hour), '%H %p').time()
-        else:
-            # If user doesn't specify am/pm, choose most logical
-            if int(hour) in list(range(1,7)): # Likely PM
-                scheduled_time = datetime.strptime('{} PM'.format(hour), '%H %p').time()
+        # return date
+        return scheduled_date
+
+    def parse_time(self, text):
+        '''
+        Parses time from human-readable text
+        Does not deal with minutes yet
+        '''
+
+        scheduled_time = datetime.strptime('12:00', '%H:%M').time()
+
+        current_time = datetime.now().time()
+
+        try:
+            ints_str = [str(i) for i in list(range(1, 25))]
+            if re.findall('at ?\d+\s?a?p?m?.*', text.lower()):
+                hour =  re.findall('at ?\d+\s?a?p?m?.*', text.lower())
+                hour = hour[0].replace('at', '').replace('pm', '').replace('am', '').replace(' ', '')
+            elif re.findall(' ?\d+ *a?p?m?.*', text.lower()):
+                hour = re.findall(' ?\d+\s?a?p?m?.*', text.lower())
+                hour = hour[0].replace('at', '').replace('pm', '').replace('am', '').replace(' ', '')
             else:
-                scheduled_time = datetime.strptime('{} AM'.format(hour), '%H %p').time()
-    except Exception as e:
-        print(e)
+                return scheduled_time
 
-    return scheduled_time
+            if 'pm' in text:
+                scheduled_time = datetime.strptime('{} PM'.format(hour), '%I %p').time()
+            elif 'am' in text:
+                scheduled_time = datetime.strptime('{} AM'.format(hour), '%I %p').time()
+            else:
+                # If user doesn't specify am/pm, choose most logical
+                if int(hour) in list(range(1,7)): # Likely PM
+                    scheduled_time = datetime.strptime('{} PM'.format(hour), '%I %p').time()
+                else:
+                    scheduled_time = datetime.strptime('{} AM'.format(hour), '%I %p').time()
+        except Exception as e:
+            print(e)
 
-def convert_to_datetime():
-    '''
-    Converts date and time to datetime object
-    '''
-    pass
+        return scheduled_time
 
-def schedule_job(dt):
-    '''
-    Schedules a job given a datetime object
-    '''
-    pass
+    def parse_datetime(self, text):
+        '''
+        Converts date and time to datetime object
+        '''
+
+        # Parse date and time
+        scheduled_date = self.parse_date(text)
+        scheduled_time = self.parse_time(text)
+
+        # Combine date and time
+        datetime_as_str = '{}-{}-{} {}:00'.format(
+                                            scheduled_date.year,
+                                            scheduled_date.month,
+                                            scheduled_date.day,
+                                            scheduled_time.hour,
+                                            )
+        print(datetime_as_str)
+        scheduled_datetime = datetime.strptime(datetime_as_str, '%Y-%m-%d %H:%M')
+
+        return scheduled_datetime
+
+    def schedule_job(self, dt):
+        '''
+        Schedules a job given a datetime object
+        '''
+        pass
 
 if __name__ == "__main__":
 
