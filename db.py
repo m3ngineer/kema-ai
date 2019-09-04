bf23d4d9e25308126ff22fea3ca1a8d960596add
Matthwe Eng <matthew.eng2@gmail.com>
2019-09-04 08:45:03 -0400

    Add db.py to connect to db and create kema_ai table

---
+++ db.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+
+import conf
+
+def connect_to_rds(return_engine=False):
+    ''' Connects to RDS and returns connection '''
+    engine = create_engine(
+            "postgresql://{}:{}@{}/{}".format(
+                conf.RDS_user,
+                conf.RDS_password,
+                conf.RDS_host,
+                conf.RDS_db_name,
+                )
+            )
+    if return_engine:
+        return engine
+
+    conn = engine.connect()
+    return conn
+
+def create_tables(drop_table=False):
+    ''' Creates post_metric table in RDS database '''
+    conn = connect_to_rds()
+
+    if drop_table:
+        try:
+            sql = 'DROP TABLE {};'.format(table)
+            conn.execute(sql)
+            print('table {} dropped'.format(table))
+        except:
+            pass
+
+    # Create new tables
+    create_table_sql  = """
+            CREATE TABLE kema_ai (
+            id bigint PRIMARY KEY,
+            task VARCHAR,
+            barrier VARCHAR,
+            possibility VARCHAR,
+            scheduled_time_text_input VARCHAR,
+            scheduled_time TIMESTAMP
+            );
+            """
+
+    conn.execute(create_table_sql)
+    print('kema_ai table created.')
+    conn.close()
